019c90b775db90c70d6f120b3fb841a9e3eccaa2
heeezni <hjin3456@naver.com>
2025-04-10 16:43:14 +0900

    자바프로그래밍 기초 Ex

---
+++ test/src/test/Mytest.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Mytest {
+
+	public static void main(String[] args) {
+		int a = 995;
+		int num = a % 2;
+		if (num == 1) {
+		System.out.println(a + "은(는) 홀수입니다.");
+		} else {
+			System.out.println(a + "은(는) 짝수입니다.");
+	}
+		
+	}
+
+}
