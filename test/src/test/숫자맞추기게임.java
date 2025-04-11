93dbf1de6cc906d54f6891ecd0538156abc5ada2
heeezni <hjin3456@naver.com>
2025-04-11 12:24:34 +0900

    자바 프로그래밍 Ex

---
+++ test/src/test/숫자맞추기게임.java
@@ -0,0 +1,32 @@
+package test;
+import java.util.Scanner;
+
+public class 숫자맞추기게임 {
+
+	public static void main(String[] args) {
+		
+		int answer = (int)(Math.random()*100) +1; // 랜덤 수 구하기
+		int count = 0; //입력횟수 저장변수 선언 
+		
+		while (true) {
+
+				Scanner sc = new Scanner (System.in);
+				int userNum = sc.nextInt( );
+				count += 1; //값 입력시 1씩 증가 (+=: '='기준 오른쪽 값을 왼쪽에 더하고 대입)
+				
+				if (userNum > answer) {
+					System.out.println("작은 수를 입력하세요."); 
+				} else if (userNum < answer) {
+				    System.out.println("큰 수를 입력하세요."); 
+				} else {	
+					System.out.println("정답입니다 . 입력하신 횟수는 " + count + "번 입니다.");
+					break;
+				} 
+					
+				}
+				
+		
+
+
+	}
+}
